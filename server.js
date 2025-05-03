582e15ef8b339245870ba569a8fa31c329630348
reza <rezabehroozian2@gmail.com>
2025-05-03 19:44:05 +0330

    added static content to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,17 +13,24 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
+// Serve static files (like index.html) from 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// API route to receive data
+app.post("/data", (req, res) => {
+  const value = req.body.value;
+  console.log("Received:", value);
+  io.emit("newData", value); // Broadcast to all clients
+  res.sendStatus(200);
+});
+
+// Socket.IO listener
 io.on("connection", (socket) => {
   console.log("Client connected");
 });
 
-app.post("/data", (req, res) => {
-  const value = req.body.value;
-  console.log("Received:", value);
-  io.emit("newData", value); // broadcast to clients
-  res.sendStatus(200);
-});
-
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+// Use Railway-provided port
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
