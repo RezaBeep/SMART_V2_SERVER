30a38837c40dfa1d25a44896884b3431952f3ffe
reza <rezabehroozian2@gmail.com>
2025-07-16 12:38:25 +0330

    changed server ip

--- server.js
+++ server.js
@@ -73,6 +73,6 @@ server.get("/api/data", (req, res) => {
 
 // Use Railway-provided port
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
